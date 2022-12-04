9b57fb391cc529d31beabebaebd6ed3b3047fac8
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-12-04 16:21:44 -0300

    feat: Navbar alinhada e estilização iniciada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,55 @@
 import React from 'react';
+import '../styles/components/Navbar.scss';
+
+const navLinks = [
+  {
+    id: 1,
+    title: 'Home',
+    path: '/',
+  },
+  {
+    id: 2,
+    title: 'Expertise',
+    path: '/#',
+  },
+  {
+    id: 3,
+    title: 'Experience',
+    path: '/#',
+  },
+  {
+    id: 4,
+    title: 'Projects',
+    path: '/#',
+  },
+  {
+    id: 5,
+    title: 'Contact',
+    path: '/#',
+  },
+];
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="Navbar">
+      <div id="logo">
+        <a href="/#">Logo</a>
+      </div>
+
+      <div id="nav">
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={link.path}>
+                {
+                link.title
+              }
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   );
 }
 
